3bbe3d0a139e255ccc2504b9dc8d526361f7203b
ayeshatajarab <A.tajarab6656@student.leedsbeckett.ac.uk>
2026-03-27 15:43:30 +0000

    I have added the manualEntry + OcrFailure file into the Routes file

--- app/src/main/java/com/example/impulsepurchaserecoverykit/navigation/Routes.kt
+++ app/src/main/java/com/example/impulsepurchaserecoverykit/navigation/Routes.kt
@@ -1,4 +1,7 @@
 package com.example.impulsepurchaserecoverykit.navigation
+
+import com.example.impulsepurchaserecoverykit.ui.screens.OcrFailureReason
+
 sealed class Screen(val route: String) {
     data object Splash : Screen("splash")
     data object Onboarding : Screen("onboarding")
@@ -7,6 +10,10 @@ sealed class Screen(val route: String) {
     data object Stats : Screen("stats")
     data object Scan : Screen("scan")
     data object Bot : Screen("bot")
+    data object ManualEntry : Screen("manual_entry")
+    data object OcrFailure : Screen("ocr_failure/{reason}"){
+        fun create(reason : OcrFailureReason) = "ocr_failure/${reason.name}"
+    }
     data object ReceiptDetail : Screen("receipt/{receiptId}") {
         fun create(receiptId: Long) = "receipt/$receiptId"
     }
