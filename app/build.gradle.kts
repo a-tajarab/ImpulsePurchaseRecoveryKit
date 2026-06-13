5163958f0243c6b91af853a9c9cc4275839a85e9
ayeshatajarab <A.tajarab6656@student.leedsbeckett.ac.uk>
2026-06-13 16:47:53 +0100

    uploaded Dokka

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.serialization") version "1.9.0"
+    id("org.jetbrains.dokka")
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.ksp)
 }
